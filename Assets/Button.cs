65a25d92258c6c3465d0c5b1763f0ee54743465f
Tw0DEE <feelgodinc.2dandteabeg@gmail.com>
2020-04-22 19:48:53 +0500

    дверь и кнопка

---
+++ Assets/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public bool pressed;
+    public Door door1;
+
+     private void OnTriggerEnter2D(Collider2D other) 
+     {
+        enabled = other.gameObject.tag == "Player";
+     }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+      door1.isOpened = pressed;
+    }
+    
+}
