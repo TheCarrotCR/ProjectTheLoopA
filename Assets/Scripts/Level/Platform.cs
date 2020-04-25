5f35aca5a65d7f69f1083ed069675f9c3cc44b81
TheCarrotCR <sevligr@gmail.com>
2020-04-26 04:58:10 +0500

    Коллизии игрока теперь работают и д е а л ь н о

--- Assets/Scripts/Level/Platform.cs
+++ Assets/Scripts/Level/Platform.cs
@@ -11,4 +11,14 @@ public class Platform : MonoBehaviour
         if (other.gameObject.GetComponent<ClimbyThing>() != null)
             passable = true;
     }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Phantom")
+        {
+            if (other.gameObject.GetComponent<Player>().CanClimb())
+                return;
+            var distance = other.collider.Distance(other.otherCollider);
+            other.transform.Translate(distance.normal * distance.distance  - distance.normal * 0.0001f);
+        }
+    }
 }
--- Assets/Scripts/Level/Wall.cs
+++ Assets/Scripts/Level/Wall.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Phantom")
+        {
+            var distance = other.collider.Distance(other.otherCollider);
+            other.transform.Translate(distance.normal * distance.distance - distance.normal * 0.0001f);
+        }
+    }
 }
